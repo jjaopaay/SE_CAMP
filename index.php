c93552e3503a00dfe968d3bd11115c6d35a05b03
jjaopaay <spykid951@gmail.com>
2023-12-21 19:32:35 +0700

    yps

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
+
+
 <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
     <body>
-        <h1>This PHP <?phpecho 'hello Wolrd!';
-        ?></h1>
-        <div>
-        <?php
-        { die;
-        ?>
-        <p>2</p>
-        <?php
-        }
-        ?>
-        <p>3</p>
-        </div>
+        <h1><?php echo "Hello World<br><br>" ;?></h1>
+        <?php echo "<h1>Hello World<br><br></h1>" ?>
+        <?php $var_a = "SE CAMP" ?>
+        <?php echo $var_b = 'Hello $var_a' ?>
+        <br>
+         <?php echo $var_c = "Hello $var_a" ?>
+         <br>
+         <?php 
+         $var_c = $var_b =1;
+         if($var_b === $var_c) {
+            echo'$var_0b === $var_c';
+
+         }else if($var_b <> $var_c){
+echo'$var_b <> $var_c';
+         }else {
+            echo 'else';
+         } ?>
+         
     </body>
-</html>
+</html> 
---
+++ index_homework.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <h1>This PHP <?phpecho 'hello Wolrd!';
+        ?></h1>
+        <div>
+        <?php
+        { 
+            #die;
+            //
+            /*
+            */
+        ?>
+        <p>2</p>
+        <?php
+        }
+        ?>
+        <p>3</p>
+        </div>
+    </body>
+</html>
