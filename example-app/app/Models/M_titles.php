9f77c3ba66d809ec2b91e01f2a70028000fe1006
jjaopaay <spykid951@gmail.com>
2024-01-15 18:22:25 +0700

    spy

---
+++ example-app/app/Models/M_titles.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class M_titles extends Model
+{
+    use HasFactory;
+
+    protected $table='title';
+    protected $primarykey='tit_id';
+    public $incrementing=true;
+    public $timestamp=false;
+}
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,9 +1,8 @@
 <?php
-
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Htpp\Controllers\C_titles;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,34 +14,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::resource('titles', C_titles::class);
 
-Route::get('/my-controller', [MyController::class, 'index']);
+Route::get('/my-controller',[MyController::class,'index']);
+Route::get('/my-controller2','App\Htpp\Controllers\MtController@index');
 
-Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
-Route::namespace('App\Http\Controllers')->group(function(){
-    Route::get('/my-controller3', 'MyController@index');
-    Route::post('/my-controller3-post', 'MyController@store');
+Route::namespace('App\Htpp\Controllers')->group(function(){
+    Route::get('\my-controller3','MyController@index');
 });
 
-Route::resource('/my-controller4', MyController::class);
-
-
+Route::resource('/my-controller4','MyController');
 Route::get('/', function () {
-    return view('welcome'); // welcome.blade.php
+    return view('welcome');
 });
-
-// use Illuminate\Http\Request;
-
-Route::get('/my-route', function(){
-    // return view('myroute');
-    //        Key    =>  Value
-    $data = ['val_a' => 'Hello World!'];
-    $data['val_b'] = "Laravel";
-    return view('myfolder.mypage',$data);
+Route::get('/my-route',function(){
+    $data=['val_a' => 'Hello World!'];
+    $data['val_b'] = " Laravel";
+   return view('myfolder.mypage',$data);
 });
-
-
-Route::post('/my-route', function(Request $req){
-    $data['myinput'] =  $req->input('myinput');
-    return view('myroute', $data);
+Route::post('/my-route',function(Request $req){
+    $data['number'] = $req->input('number');
+    return view('myroute',$data);
 });
