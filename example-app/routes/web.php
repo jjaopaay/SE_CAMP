5f80a906f7804d5a956b9b48e64a9489814e595b
jjaopaay <spykid951@gmail.com>
2024-01-29 19:21:36 +0700

    spy

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\C_titles;
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyAuth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('titles', C_titles::class);
 
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
+
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    //auth first
+});
 Route::get('/my-controller', [MyController::class, 'index']);
 
 Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
@@ -31,7 +40,7 @@ Route::resource('/my-controller4', MyController::class);
 
 
 Route::get('/', function () {
-    return view('welcome'); // welcome.blade.php
+    return view('myfolder.welcome'); // welcome.blade.php
 });
 
 // use Illuminate\Http\Request;
